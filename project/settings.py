578ebc109a9efef21d5739e6375dca60ae83bc83
jotadiasss <joaopefdias@gmail.com>
2025-11-05 10:03:09 -0300

    ajeitei o site

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # OBSERVAÇÃO: Adicionadas URLs de redirecionamento de Login e Logout
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/' # Esta linha envia o usuário para a home após o logout
