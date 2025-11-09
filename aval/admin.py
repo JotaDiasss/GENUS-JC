fedddfd698f5536240ca529478b5d7adb7b8b6a6
arthurmarmed <arthurmarmed1234@gmail.com>
2025-11-09 16:13:56 -0300

    ver o feedback no admin

--- aval/admin.py
+++ aval/admin.py
@@ -1,7 +1,15 @@
-
-
-# Register your models here.
 from django.contrib import admin
-from .models import feedback
+from .models import SiteFeedback, feedbackModel 
 
-admin.site.register(feedback)
+@admin.register(SiteFeedback)
+class SiteFeedbackAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_criacao', 'email') 
+    list_filter = ('data_criacao',) 
+    search_fields = ('nome', 'email', 'mensagem') 
+    readonly_fields = ('nome', 'email', 'mensagem', 'data_criacao') 
+
+@admin.register(feedbackModel)
+class FeedbackModelAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'user', 'data', 'estrelas')
+    list_filter = ('data', 'estrelas')
+    search_fields = ('titulo', 'user', 'detalhes')
